2ff6662b153b4a6292a8d5f05f07f359723ad777
Ankit Saini <ankitsaini0010@gmail.com>
2017-09-03 19:49:30 -0400

    Added sphere
    test

--- java_cad_practice.groovy
+++ java_cad_practice.groovy
@@ -1,5 +1,2 @@
-return new Cube(	40,// X dimention
-			60,// Y dimention
-			80//  Z dimention
-			).toCSG()// this converts from the geometry to an object we can work with
 
+return [new Sphere(40).toCSG() ,  new Cube(40,60,80).toCSG()]
