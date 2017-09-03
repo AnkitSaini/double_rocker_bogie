32cf6d49b1fae1277947fb8fc8752dca8d8d8267
Ankit Saini <ankitsaini0010@gmail.com>
2017-09-03 19:58:34 -0400

    Moved cube and sphere
    test

--- java_cad_practice.groovy
+++ java_cad_practice.groovy
@@ -1,2 +1,8 @@
 
-return [new Sphere(40).toCSG() ,  new Cube(40,60,80).toCSG()]
+CSG mysphere = new Sphere(40).toCSG()
+CSG mycube = new Cube(40,60,80).toCSG()
+
+CSG movedCube = mycube.movex(50).movey(50)
+mysphere = mysphere.movez(50)
+
+return [ mysphere, mycube, movedCube ]
