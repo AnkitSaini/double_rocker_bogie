15205633987ccde1c9d2bfbcab24b4aed3bc628b
Ankit Saini <ankitsaini0010@gmail.com>
2017-10-09 14:02:45 -0400

    Changed Body

--- bodyCad.groovy
+++ bodyCad.groovy
@@ -5,6 +5,7 @@ import com.neuronrobotics.bowlerstudio.vitamins.*;
 import java.nio.file.Paths;
 import eu.mihosoft.vrl.v3d.FileUtil;
 import com.neuronrobotics.bowlerstudio.vitamins.*;
+import com.neuronrobotics.bowlerstudio.physics.*;
 println "Loading STL file"
 // Load an STL file from a git repo
 // Loading a local file also works here
@@ -16,88 +17,37 @@ return new ICadGenerator(){
 	public ArrayList<CSG> generateBody(MobileBase b ) {
 		ArrayList<CSG> allCad=new ArrayList<>();
 		double size =40;
-
-		File mainBodyFile = ScriptingEngine.fileFromGit(
-			"https://github.com/NeuronRobotics/NASACurisoity.git",
-			"STL/body.STL");
-		File USP1R_file = ScriptingEngine.fileFromGit(
-			"https://github.com/NeuronRobotics/NASACurisoity.git",
-			"STL/upper-suspension-p1-right.STL");
-		File LSP1R_file = ScriptingEngine.fileFromGit(
-			"https://github.com/NeuronRobotics/NASACurisoity.git",
-			"STL/lower-suspension-p1-right.STL");
-		File USP2R_file= ScriptingEngine.fileFromGit(
-			"https://github.com/NeuronRobotics/NASACurisoity.git",
-			"STL/upper-suspension-p2-right.STL");
+		CSG body = new Cube(size).toCSG();
+		ArrayList<CSG> attach = [];
+		for(DHParameterKinematics l:b.getAppendages()){
+			TransformNR position = l.getRobotToFiducialTransform();
+			Transform csgTrans = TransformFactory.nrToCSG(position)
+			CSG attachment = body// get attachment for root
+				.transformed(csgTrans)
+				.setColor(javafx.scene.paint.Color.RED)
+			attach.add(attachment);
+		}
+	 	for(DHParameterKinematics l:b.getSteerable()){
+			TransformNR position = l.getRobotToFiducialTransform();
+			Transform csgTrans = TransformFactory.nrToCSG(position)
+			CSG attachment = body// get attachment for root
+				.transformed(csgTrans)
+				.setColor(javafx.scene.paint.Color.WHITE)
+			attach.add(attachment);
+		}
+		for(DHParameterKinematics l:b.getDrivable()){
+			TransformNR position = l.getRobotToFiducialTransform();
+			Transform csgTrans = TransformFactory.nrToCSG(position)
+			CSG attachment = body// get attachment for root
+				.transformed(csgTrans)
+				.setColor(javafx.scene.paint.Color.BLUE)
+			attach.add(attachment);
+		}
 		
-		File USP1L_file = ScriptingEngine.fileFromGit(
-			"https://github.com/NeuronRobotics/NASACurisoity.git",
-			"STL/upper-suspension-p1-left.STL");
-		File LSP1L_file= ScriptingEngine.fileFromGit(
-			"https://github.com/NeuronRobotics/NASACurisoity.git",
-			"STL/lower-suspension-p1-left.STL");
-		File USP2L_file = ScriptingEngine.fileFromGit(
-			"https://github.com/NeuronRobotics/NASACurisoity.git",
-			"STL/upper-suspension-p2-left.STL");
-			
-		//changed
-		File LSP2R_file = ScriptingEngine.fileFromGit(
-			"https://github.com/NeuronRobotics/NASACurisoity.git",
-			"STL/lower-suspension-p2-right.STL");		
-		File LSP2L_file= ScriptingEngine.fileFromGit(
-			"https://github.com/NeuronRobotics/NASACurisoity.git",
-			"STL/lower-suspension-p2-left.STL");	
-		/*
-		double upperHeightAdjust = 2.175
-		double lowerAdjustAngle = 4.6
-		double UpperOffset = -78.5
-		double heightOfUpperHinge = 27+upperHeightAdjust
-		double offsetOfLowerBracket = 8.5
-		double LowerOffset  = 65.25
-		double mainWheelAlignemnt=-5
-		double lowerHingeOffset = 1
-		*/
-		double widthOfBody =43
-	
-		// Load the .CSG from the disk and cache it in memory
-		CSG body  = Vitamins.get(mainBodyFile)
-
-		CSG USP1R  = Vitamins.get(USP1R_file).movey(widthOfBody)
-		CSG LSP1R  = Vitamins.get(LSP1R_file).movey(widthOfBody)
-		CSG USP2R  = Vitamins.get(USP2R_file).movey(widthOfBody)
-		CSG LSP2R  = Vitamins.get(LSP2R_file).movey(widthOfBody)
-		CSG USP1L  = Vitamins.get(USP1L_file).movey(-widthOfBody)
-		CSG LSP1L  = Vitamins.get(LSP1L_file).movey(-widthOfBody)
-		CSG USP2L  = Vitamins.get(USP2L_file).movey(-widthOfBody)
-		CSG LSP2L  = Vitamins.get(LSP2L_file).movey(-widthOfBody)
-
-
-/*
-		FileUtil.write(Paths.get(USP1L_file.getAbsolutePath()),
-							USP1L.toStlString());
-		FileUtil.write(Paths.get(USP2L_file.getAbsolutePath()),
-							USP2L.toStlString());
-		FileUtil.write(Paths.get(LSP1L_file.getAbsolutePath()),
-							LSP1L.toStlString());
-		FileUtil.write(Paths.get(LSP2L_file.getAbsolutePath()),
-							LSP2L.toStlString());
-		//right					
-		FileUtil.write(Paths.get(USP1R_file.getAbsolutePath()),
-							USP1R.toStlString());
-		FileUtil.write(Paths.get(USP2R_file.getAbsolutePath()),
-							USP2R.toStlString());
-		FileUtil.write(Paths.get(LSP1R_file.getAbsolutePath()),
-							LSP1R.toStlString());
-		FileUtil.write(Paths.get(LSP2R_file.getAbsolutePath()),
-							LSP2R.toStlString());
-*/
 		body.setManipulator(b.getRootListener());
 		body.setColor(javafx.scene.paint.Color.WHITE)
-		def parts = [body ,USP1R,LSP1R,USP2R,LSP2R,USP1L,LSP1L,USP2L,LSP2L ] as ArrayList<CSG>
-		
-		for(int i=1;i<parts.size();i++){
-			parts.get(i).setColor(javafx.scene.paint.Color.GRAY)
-		}
+		def parts = [body]		
+		parts.addAll(attach);
 		return parts;
 	}
 };
--- rockerBogieFactory.groovy
+++ rockerBogieFactory.groovy
@@ -1,5 +1,5 @@
 //Your code here
-
+/*
 CSG rockerBogie(){
 	rockerLength = new LengthParameter("Rocker Link Length",100,[500.0, 10.0])
 	rockerWidth = new LengthParameter("Rocker Link Width",10,[100.0, 1.0])
@@ -53,3 +53,34 @@ CSG rockerBogie(){
 
 CSGDatabase.clear()
 return rockerBogie()
+*/
+
+//Testing some features to understand
+import com.neuronrobotics.bowlerstudio.creature.ICadGenerator;
+import com.neuronrobotics.bowlerstudio.creature.CreatureLab;
+import org.apache.commons.io.IOUtils;
+//Create the kinematics model from the xml file describing the D-H compliant parameters. 
+def file=["https://github.com/madhephaestus/carl-the-hexapod.git","CarlTheRobot.xml"]as String[]
+String xmlContent = ScriptingEngine.codeFromGit(file[0],file[1])[0]
+
+println "Loading the robot"
+MobileBase base=null;
+if(DeviceManager.getSpecificDevice(MobileBase.class, "CarlTheWalkingRobot")==null){
+	BowlerStudio.speak("Connecting CarlTheWalkingRobot.");
+	base = new MobileBase(IOUtils.toInputStream(xmlContent, "UTF-8"));
+	DeviceManager.addConnection(base,"CarlTheWalkingRobot");
+}else{
+	base = (MobileBase)DeviceManager.getSpecificDevice(MobileBase.class, "CarlTheWalkingRobot");
+}
+
+
+def script = ["https://gist.github.com/e54cfebe4f55fb0549dd.git","ExampleCadGenerator.groovy"]as String[]
+base.setGitCadEngine(script)
+for(DHParameterKinematics appendge: base.getAllDHChains()){
+	appendge.setGitCadEngine(script)
+}
+
+return null;
+println "***********************"
+println "***Last line reached***"
+println "***********************"
